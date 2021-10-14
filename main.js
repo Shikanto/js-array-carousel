116735b8fe1aef8d29b836af88c187f5069153ee
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-14 17:08:09 +0200

    progetto statico cambiato

---
+++ main.js
@@ -0,0 +1,36 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+const mainImg = document.querySelector(".main-img");
+const textImg = document.querySelector(".img-text");
+const sidebarContainer = document.querySelector(".sidebar-container");
+
+let currentIndex = 0;
+
+for (let i = 0; i < items.lenght; i++) {
+    
+    const imgTaken = items[i];
+
+    let imgSidebar = `<div class="sidebar-img bg-01 cover-background-img></div>`
+}
